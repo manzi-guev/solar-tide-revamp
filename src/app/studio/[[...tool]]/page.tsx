bf2f49b24a80cd11bf77d8a6b27c84581a75f238
Manzi <manziguevara@gmail.com>
2026-09-05 22:28:54 +0200

    fix(studio): force-dynamic to skip static rendering

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -3,6 +3,8 @@ import config from '../../../../sanity.config'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
+export const dynamic = 'force-dynamic'
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
